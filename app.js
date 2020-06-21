da650db992d4f55d45406922827e2c6df8e77417
Jacob Zirbel <jacobzirbel@gmail.com>
2020-06-21 15:43:01 -0500

    mvc

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
+// lists have to be separated by semi-colon, even single items
+// region must be called region
+//
 const fs = require("fs");
+const inquirer = require("inquirer");
 let data = fs.readFileSync("input.csv", "utf8");
 let [headers, ...rest] = data.split("\n");
 let template = {};
 headers.split(",").forEach((header, i) => {
   header = header.trim().toLowerCase();
   if (header.includes("max")) {
-    let key = header.substr(0, header.indexOf("max"));
+    let key = header.substr(0, header.indexOf("max")).trim();
 
     if (template[key]) {
       template[key]["max"] = i;
@@ -14,7 +18,7 @@ headers.split(",").forEach((header, i) => {
       template[key]["max"] = i;
     }
   } else if (header.includes("min")) {
-    let key = header.substr(0, header.indexOf("min"));
+    let key = header.substr(0, header.indexOf("min")).trim();
     if (template[key]) {
       template[key]["min"] = i;
     } else {
@@ -25,18 +29,79 @@ headers.split(",").forEach((header, i) => {
     template[header] = i;
   }
 });
-let chars = rest.map((char) => {
+let chars = rest
+  .map((char) => {
+    let ret = {};
+    char = char.split(",");
+    Object.keys(template).forEach((key) => {
+      if (typeof template[key] === "number") {
+        let prop = char[template[key]];
+        if (prop && prop.includes(";")) {
+          ret[key] = prop
+            .split(";")
+            .map((e) => e.trim())
+            .filter((e) => e);
+        } else {
+          ret[key] = prop;
+        }
+      } else {
+        ret[key] = {};
+        ret[key]["min"] = char[template[key]["min"]];
+        ret[key]["max"] = char[template[key]["max"]];
+      }
+    });
+    if (ret.name) return ret;
+  })
+  .filter((e) => e);
+
+let regions = [];
+chars.forEach((char) => {
+  regions.push(...char.region);
+});
+regions = regions.filter((e, i) => i === regions.indexOf(e));
+let regionChars = {};
+regions.forEach((r) => {
+  let cs = chars.filter((e) => e.region.includes(r));
+  regionChars[r] = cs;
+});
+function ask(){
+  inquirer
+  .prompt([
+    {
+      name: "region",
+      message: "Which region? ",
+      type: "list",
+      choices: regions,
+    },
+  ])
+  .then((answers) => {
+    console.log(getCharByRegion(answers.region));
+    ask()
+  });
+}
+function getCharByRegion(region) {
+  let char = getRandomFromArray(regionChars[region]);
   let ret = {};
-  char = char.split(",");
-  Object.keys(template).forEach((key) => {
-    if (typeof template[key] === "number") {
-      ret[key] = char[template[key]];
+  Object.keys(char).forEach((key) => {
+    if (typeof char[key] === "object") {
+      if (char[key].length !== undefined) {
+        ret[key] = getRandomFromArray(char[key]);
+      } else {
+        ret[key] = getRndInteger(char[key].min, char[key].max);
+      }
     } else {
-      ret[key] = {}
-      ret[key]["min"] = char[template[key]["min"]];
-      ret[key]["max"] = char[template[key]["max"]];
+      ret[key] = char[key]
     }
   });
-  return ret; 
-});
-console.log(chars[0]);
+  return ret;
+}
+function getRandomFromArray(array) {
+  return array[getRndInteger(0, array.length - 1)];
+}
+function getRndInteger(min, max) {
+  // min and max included
+   min = +min
+   max = +max
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+ask();
